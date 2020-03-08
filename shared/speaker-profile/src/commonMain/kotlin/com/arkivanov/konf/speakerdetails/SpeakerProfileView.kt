7e7d5cba2b6d20be96b2fd62fdb30c51368cf84e
Arkadii Ivanov <>
2020-03-08 14:33:31 +0000

    Added additional fields for models

--- shared/speaker-profile/src/commonMain/kotlin/com/arkivanov/konf/speakerdetails/SpeakerProfileView.kt
+++ shared/speaker-profile/src/commonMain/kotlin/com/arkivanov/konf/speakerdetails/SpeakerProfileView.kt
@@ -6,8 +6,18 @@ import com.arkivanov.mvikotlin.core.view.MviView
 interface SpeakerProfileView : MviView<Model, Nothing> {
 
     data class Model(
-        val isLoading: Boolean = false,
-        val isError: Boolean = false,
-        val name: String? = null
+        val isLoading: Boolean,
+        val isError: Boolean,
+        val name: String?,
+        val imageUrl: String?,
+        val location: String?,
+        val biography: String?,
+        val twitterAccount: String?,
+        val githubAccount: String?,
+        val facebookAccount: String?,
+        val linkedInAccount: String?,
+        val mediumAccount: String?,
+        val companyName: String?,
+        val companyLogoUrl: String?
     )
 }
